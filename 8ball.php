639b426d9af2fcd1c2952ac65e84261e1ca08f73
matheushenrick <matheus.henrick@outlook.com>
2020-07-14 15:07:09 -0300

    8ball adicionado

---
+++ 8ball.php
@@ -0,0 +1,32 @@
+<?php
+$m01="OBVIAMENTE MONA!"
+$m02="Nem minha avó acredita nessa.";
+$m03="Você deveria repensar nisso.";
+$m04="Se olhe no espelho e se pergunte novamente isso antes de passar vergonha aqui.";
+$m05="Bem duvidoso.";
+$m06="Sim velho,cê credita?!?";
+$m07="Quer algo mais obvio que isso?";
+$m08="Pode ser.";
+$m09="Pergunta denovo, agora em português.";
+$m10="Se é o que vc diz, cada louco com suas teorias.";
+$m11="Nem o Lula é tão cara de pau.";
+$m12="Um cão chupando manga tem mais moral que você.";
+$m13="AH! CLARO! E EU SOU O BOZO!";
+$m14="Meu... Deus... É cada erro que o universo comete pra nascer um ser desse...";
+$m15="FAÇA ISSO! Só que em silêncio e sozinho de preferência.";
+$m16="A sua vergonha foi passada no crédito, porque você é pobre.";
+$m17="Só Deus sabe.";
+$m18="Veeeelho, quer uma bússola?";
+$m19="Concordo contigo, MestrX da razão";
+$m20="Se você falou ta falado";
+$m21="Obviamente!!";
+$m22="Mais claro que isso, só o Julienio no sol";
+$m23="Claro MIGAAAA, se eu fosse você até desfilava depois dessa";
+
+$rm = array($m01,$m02,$m03,$m04,$m05,$m06,$m07,$m08,$m09,$m10,$m11,$m12,$m13,$m14,$m15,$m16,$m17,$m18,$m19,$m20,$m21,$m22,$m23);
+
+$random_keys = array_rand($rm,1);
+
+echo $rm[$random_keys];
+
+?>
